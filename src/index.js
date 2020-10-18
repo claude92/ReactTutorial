c99a409c9a4ad9c2cbd2272066d9ac30f864a187
claudiuPunch <67048726+claudiuPunch@users.noreply.github.com>
2020-10-18 15:33:41 +0200

    elevate the state to parent

--- src/index.js
+++ src/index.js
@@ -2,29 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
-  constructor(props) {
-    //first instruction always on derived class
-    super(props);
-    //state is a private variable accessible only in the component
-    this.state = {
-      value: null
-    }
-  }
   render() {
     return (
       //funzione come prop onClick
       <button
         className="square"
-        onClick={() => this.setState({ value: 'X' })}>
-        { this.state.value}
+        onClick={() => this.props.onClick()}>
+        { this.props.value}
       </button >
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares })
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (<Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)} />);
   }
 
   render() {
