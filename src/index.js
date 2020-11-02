745f10c0082300e11d9a1d8ae9c875835d8a67fc
claudiuPunch <67048726+claudiuPunch@users.noreply.github.com>
2020-11-02 12:17:41 +0100

    concetti chiave introduzione a JSX

--- src/index.js
+++ src/index.js
@@ -2,30 +2,74 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Timer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { seconds: 0 }
-    }
-    tick() {
-        this.setState(state => { return { seconds: state.seconds + 1 } })
-    }
-    componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
-    }
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-    render() {
-        return (
-            <div>
-                Secondi: {this.state.seconds}
-            </div>
-        )
-    }
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+const user = {
+  firstName: 'Giuseppe',
+  lastName: 'Verdi'
+}
+// const element = (<h1>
+//   Hello, {formatName(user)}
+// </h1>)
+/**
+ * "" for string valutation
+ */
+//const element = <div tabIndex="0"></div>
+/**
+ * jsx in attribute html
+ */
+// const element = <img alt="ok" src={user.avatar}></img>
+/**
+ * compilation with babel
+ */
+// const element = (
+//   <h1 className="greeting">
+//     Hello world!
+//   </h1>
+// )
+// const element = React.createElement(
+//   'h1',
+//   { className: 'greeting' },
+//   'Hello, world!'
+// )
 
+/**
+ * 
+ * output of babel 
+ */
+// Nota: questa struttura è semplificata
+// const element = {
+//   type: 'h1',
+//   props: {
+//     className: 'greeting',
+//     children: 'Hello, world!'
+//   }
+// };
+
+/**
+ *
+ * jsp can contain child elements
+ */
+// const element = (
+//   <div>
+//     <h1>Hello!</h1>
+//     <h2>Good to see you here.</h2>
+//   </div>
+// );
+
+
+/**
+ * jsp in if(for),assign it,function argument,return argument
+ */
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>
+  } else {
+    return <h1>Hello, Stranger</h1>
+  }
 }
 ReactDOM.render(
-    <Timer />,
-    document.getElementById('root')
-);
+  getGreeting(user),
+  document.getElementById('root')
+)
